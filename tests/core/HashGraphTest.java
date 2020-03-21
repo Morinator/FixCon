3adef3f8473e115806f1d37bd46bab1e2ba6b06d
Moritz <mgross321@gmail.com>
2020-03-21 22:04:13 +0100

    rtcvbncvbctrzer

--- tests/core/HashGraphTest.java
+++ tests/core/HashGraphTest.java
@@ -30,54 +30,53 @@ class HashGraphTest {
         g2.addEdge(4, 5);
     }
 
-
     @Test
     void addEdge() {
-        assertFalse(g1.adjacent(2, 3));
-        assertFalse(g1.adjacent(2, 4));
+        assertFalse(g1.are_neighbours(2, 3));
+        assertFalse(g1.are_neighbours(2, 4));
         g1.addEdge(2, 3);
         g1.addEdge(2, 4);
-        assertTrue(g1.adjacent(2, 3));
-        assertTrue(g1.adjacent(2, 4));
-        assertTrue(g1.adjacent(1, 2));
+        assertTrue(g1.are_neighbours(2, 3));
+        assertTrue(g1.are_neighbours(2, 4));
+        assertTrue(g1.are_neighbours(1, 2));
 
-        assertFalse(g1.adjacent(1, 4));
-        assertFalse(g1.adjacent(5, 6));
+        assertFalse(g1.are_neighbours(1, 4));
+        assertFalse(g1.are_neighbours(5, 6));
         g1.addEdge(1, 4);
         g1.addEdge(5, 6);
         g1.addEdge(1, -100);
-        assertTrue(g1.adjacent(1, 4));
-        assertTrue(g1.adjacent(5, 6));
-        assertTrue(g1.adjacent(1, -100));
+        assertTrue(g1.are_neighbours(1, 4));
+        assertTrue(g1.are_neighbours(5, 6));
+        assertTrue(g1.are_neighbours(1, -100));
     }
 
     @Test
     void deleteNode() {
-        assertTrue(g1.hasNode(1));
-        assertTrue(g1.adjacent(1, 2));
-        assertTrue(g1.adjacent(1, 3));
-        g1.deleteNode(1);
-        g1.deleteNode(100);
-        g1.deleteNode(-100);
-        g1.deleteNode(1);
-        assertFalse(g1.hasNode(1));
-        assertFalse(g1.adjacent(1, 2));
-        assertFalse(g1.adjacent(1, 3));
-        assertFalse(g1.hasNode(-100));
+        assertTrue(g1.containsVertex(1));
+        assertTrue(g1.are_neighbours(1, 2));
+        assertTrue(g1.are_neighbours(1, 3));
+        g1.removeVertex(1);
+        g1.removeVertex(100);
+        g1.removeVertex(-100);
+        g1.removeVertex(1);
+        assertFalse(g1.containsVertex(1));
+        assertFalse(g1.are_neighbours(1, 2));
+        assertFalse(g1.are_neighbours(1, 3));
+        assertFalse(g1.containsVertex(-100));
 
-        g2.deleteNode(2);
+        g2.removeVertex(2);
         assertEquals(0, g2.degree(2));
     }
 
     @Test
     void deleteEdge() {
-        assertTrue(g1.adjacent(1, 2));
-        g1.deleteEdge(1, 2);
+        assertTrue(g1.are_neighbours(1, 2));
+        g1.removeEdge(1, 2);
         assertEquals(1, g1.degree(1));
-        assertFalse(g1.hasNode(2));
-        assertTrue(g1.hasNode(3));
-        g1.deleteEdge(1, 3);
-        assertFalse(g1.hasNode(1));
+        assertFalse(g1.containsVertex(2));
+        assertTrue(g1.containsVertex(3));
+        g1.removeEdge(1, 3);
+        assertFalse(g1.containsVertex(1));
         assertEquals(1, g1.degree(3));
     }
 
@@ -103,12 +102,12 @@ class HashGraphTest {
     @Test
     void isEmpty() {
         assertFalse(g1.isEmpty());
-        g1.deleteNode(1);
-        g1.deleteNode(2);
+        g1.removeVertex(1);
+        g1.removeVertex(2);
         assertFalse(g1.isEmpty());
-        g1.deleteNode(3);
+        g1.removeVertex(3);
         assertTrue(g1.isEmpty());
-        g1.deleteNode(4);
+        g1.removeVertex(4);
         assertTrue(g1.isEmpty());
         g1.addEdge(1, 2);
         assertFalse(g1.isEmpty());
@@ -117,61 +116,76 @@ class HashGraphTest {
     @Test
     void edgeCount() {
         assertEquals(3, g1.edgeCount());
-        g1.deleteEdge(1, 2);
+        g1.removeEdge(1, 2);
         assertEquals(2, g1.edgeCount());
-        g1.deleteNode(3);
+        g1.removeVertex(3);
         assertEquals(0, g1.edgeCount());
 
         assertEquals(9, g2.edgeCount());
-        g2.deleteNode(2);
+        g2.removeVertex(2);
         assertEquals(4, g2.edgeCount());
-        g2.deleteEdge(1, 5);
+        g2.removeEdge(1, 5);
         assertEquals(3, g2.edgeCount());
     }
 
     @Test
-    void getNodes() {
-        assertEquals(Set.of(1,2,3,4), g1.getNodes());
-        g1.deleteEdge(1,2);
-        assertEquals(Set.of(1,3,4), g1.getNodes());
-        g1.deleteNode(3);
-        assertEquals(Set.of(), g1.getNodes());
+    void getVertices() {
+        assertEquals(Set.of(1,2,3,4), g1.getVertices());
+        g1.removeEdge(1,2);
+        assertEquals(Set.of(1,3,4), g1.getVertices());
+        g1.removeVertex(3);
+        assertEquals(Set.of(), g1.getVertices());
 
-        assertEquals(Set.of(1,2,3,4,5,6), g2.getNodes());
-        g2.deleteNode(2);
-        assertEquals(Set.of(1,3,4,5,6), g2.getNodes());
-        g2.deleteNode(5);
-        assertEquals(Set.of(3,4,6), g2.getNodes());
-        g2.deleteEdge(3,4);
-        assertEquals(Set.of(3, 6), g2.getNodes());
-        g2.deleteEdge(3,6);
-        assertEquals(new HashSet<>(), g2.getNodes());
+        assertEquals(Set.of(1,2,3,4,5,6), g2.getVertices());
+        g2.removeVertex(2);
+        assertEquals(Set.of(1,3,4,5,6), g2.getVertices());
+        g2.removeVertex(5);
+        assertEquals(Set.of(3,4,6), g2.getVertices());
+        g2.removeEdge(3,4);
+        assertEquals(Set.of(3, 6), g2.getVertices());
+        g2.removeEdge(3,6);
+        assertEquals(new HashSet<>(), g2.getVertices());
     }
 
     @Test
-    void nodeCount() {
-        assertEquals(4, g1.nodeCount());
-        g1.deleteEdge(1,2);
-        assertEquals(3, g1.nodeCount());
-        g1.deleteNode(3);
-        assertEquals(0, g1.nodeCount());
+    void vertexCount() {
+        assertEquals(4, g1.vertexCount());
+        g1.removeEdge(1,2);
+        assertEquals(3, g1.vertexCount());
+        g1.removeVertex(3);
+        assertEquals(0, g1.vertexCount());
 
-        assertEquals(6, g2.nodeCount());
-        g2.deleteEdge(1, 2);
-        assertEquals(6, g2.nodeCount());
-        g2.deleteNode(5);
-        assertEquals(4, g2.nodeCount());
-        g2.deleteNode(4);
-        assertEquals(3, g2.nodeCount());
+        assertEquals(6, g2.vertexCount());
+        g2.removeEdge(1, 2);
+        assertEquals(6, g2.vertexCount());
+        g2.removeVertex(5);
+        assertEquals(4, g2.vertexCount());
+        g2.removeVertex(4);
+        assertEquals(3, g2.vertexCount());
     }
 
     @Test
     void getNeighbours() {
         assertEquals(Set.of(2, 3), g1.getNeighbours(1));
         assertEquals(Set.of(1), g1.getNeighbours(2));
-        g1.deleteNode(2);
+        g1.removeVertex(2);
         assertEquals(Set.of(3), g1.getNeighbours(1));
         assertEquals(new HashSet<>(), g1.getNeighbours(2));
         assertEquals(Set.of(1,4), g1.getNeighbours(3));
+    }
+
+    @Test
+    void remove_vertices_if_isolated() {
+        g1.removeEdge(3, 4);
+        g1.removeEdge(3, 1);
+        //vertices 3 and 4 are now isolated
+        assertEquals(2, g1.vertexCount());
+
+        g2.removeEdge(1, 2);
+        g2.removeEdge(1, 5);
+        assertEquals(5, g2.vertexCount());
+        g2.removeEdge(4, 5);
+        g2.removeEdge(2, 5);
+        assertEquals(4, g2.vertexCount());
     }
 }
