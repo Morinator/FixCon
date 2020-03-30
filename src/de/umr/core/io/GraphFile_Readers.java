271552a8feeda8aacda21ab98869d068cf85fa8c
Moritz <mgross321@gmail.com>
2020-03-30 18:15:41 +0200

    String-constants LINE_DATA_FORMAT and SEPARATOR are now local variables in the method they are used in and not class fields.

--- src/de/umr/core/io/GraphFile_Readers.java
+++ src/de/umr/core/io/GraphFile_Readers.java
@@ -8,17 +8,20 @@ import java.util.List;
 
 import static java.util.stream.Collectors.toList;
 
+/**This class exclusively contains static methods for reading in graph-files in different formats.
+ */
 public class GraphFile_Readers {
-
-    //captures only lines that consist of two positive integer numbers separated by spaces (each line encodes an edge)
-    private static final String LINE_DATA_FORMAT = "\\d+\\s+\\d+";
-    private static final String SEPARATOR = "\\s+";
     
     /**@param filePath String of the path of the file. For the most files,
      * you can use ".//data.fileName" (at least in the default project-structure).
      * @return Each edge is an Integer-Array of size 2. A collection of these edges specifies the whole graph.
      */
     public static List<Integer[]> readFile_NetworkRepository_Format(String filePath) throws IOException {
+
+        //captures only lines that consist of two positive integer numbers separated by spaces (each line encodes an edge)
+        final String LINE_DATA_FORMAT = "\\d+\\s+\\d+";
+        final String SEPARATOR = "\\s+";
+
         return Files.lines(Paths.get(filePath))
                 .filter(str -> str.matches(LINE_DATA_FORMAT))
                 .map(str -> Arrays.stream(str.split(SEPARATOR)).map(Integer::parseInt).toArray(Integer[]::new))
