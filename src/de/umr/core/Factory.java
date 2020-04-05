86efc5de9bbb61cf7240bbb22b8aa771adf86799
Moritz <mgross321@gmail.com>
2020-04-05 03:30:10 +0200

    refactoring. Also deleted the biMap as its useless at the moment and easy to implement.

--- src/de/umr/core/Factory.java
+++ src/de/umr/core/Factory.java
@@ -1,14 +1,12 @@
 package de.umr.core;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
+import com.google.common.graph.EndpointPair;
 import com.google.common.graph.GraphBuilder;
 import com.google.common.graph.MutableGraph;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -18,45 +16,28 @@ import static java.util.stream.Collectors.toList;
  */
 public final class Factory {
 
+    private final static String lineDataFormat_NetworkRepo = "\\d+\\s+\\d+";
+    private final static String separator_NetworkRepo = "\\s+";
+
+
     /**
      * @param edgeList String of the path of the file. For the most files,
      *                 you can use ".//data.fileName" (at least in the default project-structure).
      * @return Each edge is an Integer-Array of size 2. A collection of these edges specifies the whole graph.
      */
-    public static MutableGraph<Integer> graph_from_NetworkRepo(final List<Integer[]> edgeList) {
-        throw_error_if_edgeList_invalid(edgeList);
+    public static MutableGraph<Integer> graph_from_edgeList(final List<EndpointPair<Integer>> edgeList) {
+        //throw_error_if_edgeList_invalid(edgeList);
         final MutableGraph<Integer> result_graph = GraphBuilder.undirected().build();
-        edgeList.forEach(x -> result_graph.putEdge(x[0], x[1]));
+        edgeList.forEach(x -> result_graph.putEdge(x.nodeU(), x.nodeV()));
         return result_graph;
     }
 
-    public static List<Integer[]> edges_from_path(final String filePath) {
+    public static List<EndpointPair<Integer>> edgeList_from_NetworkRepo(final String filePath) throws IOException {
         //captures only lines that consist of two positive integer numbers separated by whitespace (each line encodes an edge)
-        final String LINE_DATA_FORMAT = "\\d+\\s+\\d+";
-        final String SEPARATOR = "\\s+";
-        try {
-            return Files.lines(Paths.get(filePath))
-                    .filter(str -> str.matches(LINE_DATA_FORMAT))
-                    .map(str -> Arrays.stream(str.split(SEPARATOR)).map(Integer::parseInt).toArray(Integer[]::new))
-                    .collect(toList());
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    static BiMap<String, Integer> vertex_to_ID(final List<String[]> edgeList) {
-        throw_error_if_edgeList_invalid(edgeList);
-        final BiMap<String, Integer> biMap = HashBiMap.create(edgeList.size());   //estimates capacity
-        for (String[] edge : edgeList) {
-            for (int i = 0; i <= 1; i++)
-                biMap.putIfAbsent(edge[i], biMap.size());
-        }
-        return biMap;
-    }
-
-    private static <T> void throw_error_if_edgeList_invalid(final List<T[]> edgeList) {
-        if (edgeList.stream().anyMatch(x -> x.length != 2))
-            throw new IllegalArgumentException("All elements must have 2 elements to represent an edge");
+        return Files.lines(Paths.get(filePath))
+                .filter(str -> str.matches(lineDataFormat_NetworkRepo))
+                .map(str -> str.split(separator_NetworkRepo))
+                .map(arr -> EndpointPair.unordered(Integer.valueOf(arr[0]), Integer.valueOf(arr[1])))
+                .collect(toList());
     }
 }
