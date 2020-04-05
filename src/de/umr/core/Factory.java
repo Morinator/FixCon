f6b16e08a93a4c21e1716bab8e0819df1d4bdc5f
Moritz <mgross321@gmail.com>
2020-04-05 02:18:21 +0200

    class now final throws exceptions earlier for wrong inputs with better messages

--- src/de/umr/core/Factory.java
+++ src/de/umr/core/Factory.java
@@ -16,7 +16,7 @@ import static java.util.stream.Collectors.toList;
 /**
  * This class exclusively contains static methods for reading in graph-files in different formats.
  */
-public class Factory {
+public final class Factory {
 
     /**
      * @param edgeList String of the path of the file. For the most files,
@@ -24,6 +24,7 @@ public class Factory {
      * @return Each edge is an Integer-Array of size 2. A collection of these edges specifies the whole graph.
      */
     public static MutableGraph<Integer> graph_from_NetworkRepo(final List<Integer[]> edgeList) {
+        throw_error_if_edgeList_invalid(edgeList);
         final MutableGraph<Integer> result_graph = GraphBuilder.undirected().build();
         edgeList.forEach(x -> result_graph.putEdge(x[0], x[1]));
         return result_graph;
@@ -45,11 +46,17 @@ public class Factory {
     }
 
     static BiMap<String, Integer> vertex_to_ID(final List<String[]> edgeList) {
+        throw_error_if_edgeList_invalid(edgeList);
         final BiMap<String, Integer> biMap = HashBiMap.create(edgeList.size());   //estimates capacity
         for (String[] edge : edgeList) {
             for (int i = 0; i <= 1; i++)
                 biMap.putIfAbsent(edge[i], biMap.size());
         }
         return biMap;
+    }
+
+    private static <T> void throw_error_if_edgeList_invalid(final List<T[]> edgeList) {
+        if (edgeList.stream().anyMatch(x -> x.length != 2))
+            throw new IllegalArgumentException("All elements must have 2 elements to represent an edge");
     }
 }
