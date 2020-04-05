dd80058d1f6e04c67c6839d8167247e107cb8aac
Moritz <mgross321@gmail.com>
2020-04-05 02:17:22 +0200

    added private constructor

--- src/de/umr/core/utils/CollectionUtil.java
+++ src/de/umr/core/utils/CollectionUtil.java
@@ -5,17 +5,22 @@ import java.util.Set;
 
 import static java.util.stream.Collectors.joining;
 
-public class CollectionUtil {
+public final class CollectionUtil {
+
+    private CollectionUtil() { //is not supposed to be instantiated
+    }
 
     static String string_SetOfSets(final Set<Set<Integer>> setOfSets) {
         return setOfSets.stream()
-            .map(i -> i.stream().sorted().map(String::valueOf).collect(joining(" ")))
-            .collect(joining("\n"));
+                .map(i -> i.stream().sorted().map(String::valueOf).collect(joining(" ")))
+                .collect(joining("\n"));
     }
 
-    /**removes the last N elements from the list. Throws indexOutOfBoundsExceptions if N is larger than list size*/
+    /**
+     * removes the last N elements from the list. Throws indexOutOfBoundsExceptions if N is larger than list size
+     */
     public static void list_remove_lastN(final List input_list, final int N) {
         for (int i = 0; i < N; i++)
-            input_list.remove(input_list.size()-1);
+            input_list.remove(input_list.size() - 1);
     }
 }
