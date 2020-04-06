83d02b49bd3e079e081b871f8e7381578b726169
Moritz <mgross321@gmail.com>
2020-04-07 00:56:02 +0200

    function hasTriangle now works and is tested

--- src/de/umr/core/GraphStatics.java
+++ src/de/umr/core/GraphStatics.java
@@ -2,7 +2,7 @@ package de.umr.core;
 
 import com.google.common.graph.Graph;
 
-public class GraphsStatics {
+public class GraphStatics {
 
     static boolean areOpenTwins (Graph<Integer> g, int x, int y) {
         return true;
@@ -18,5 +18,11 @@ public class GraphsStatics {
 
     static int diameter(Graph<Integer> g) {
         return -123;
+    }
+
+    public static boolean hasTriangle(Graph<Integer> g) {
+        return g.nodes().stream().anyMatch(x -> g.adjacentNodes(x).stream().anyMatch(
+                y -> g.adjacentNodes(x).stream().anyMatch(z -> g.hasEdgeConnecting(y, z)))
+        );
     }
 }
