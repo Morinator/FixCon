e54cabbcb4f6c8d9b4ee3a633b789aaed20569af
Morinator <mgross321@gmail.com>
2020-04-08 00:12:00 +0200

    TestClass no longer tracked in git

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,14 +1,5 @@
-import com.google.common.graph.GraphBuilder;
-import com.google.common.graph.MutableGraph;
-
-import java.io.IOException;
-
 public class TestClass {
 
-    public static void main(String[] args) throws IOException {
-        MutableGraph<Integer> g = GraphBuilder.undirected().build();
-        g.putEdge(1, 2);
-        g.putEdge(2, 1);
-        System.out.println(g.degree(1));
+    public static void main(String[] args) {
     }
 }
