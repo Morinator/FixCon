7de7ef2882bc7e436e0f49116edb8bf6260257c6
Morinator <mgross321@gmail.com>
2020-04-09 00:15:26 +0200

    This class brings together most other classes. It contains the problem and iterates of its subgraphs and stores, which of these subgraphs has the optimal target-function.
    So far it only has 1 method, getSolution, which returns an object of type solution, corresponding to the problem this class contains.

---
+++ src/de/umr/fixcon/Solver.java
@@ -0,0 +1,26 @@
+package de.umr.fixcon;
+
+import com.google.common.graph.Graphs;
+
+public class Solver {
+    Problem problem;
+    SubIter iter;
+    double globalOptimum;
+
+    public Solver(Problem problem) {
+        this.problem = problem;
+        iter = new SubIter(problem.graph, problem.subgraphSize);
+        globalOptimum = problem.function.optimum(problem.graph, problem.parameters);
+    }
+
+    public Solution getSolution() {
+        Solution solution = new Solution(null, Integer.MIN_VALUE);
+        while (solution.value < globalOptimum && iter.isValid()) {
+            if (problem.function.apply(iter.current(), problem.parameters) > solution.value) {
+                solution = new Solution(Graphs.copyOf(iter.current()), problem.function.apply(iter.current()));
+            }
+            iter.mutate();
+        }
+        return solution;
+    }
+}
