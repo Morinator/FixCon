79f461e393ce6dcd7b8ac93dc7bccda00ca1e783
Morinator <mgross321@gmail.com>
2020-04-11 15:32:33 +0200

    changed access modifiers

--- src/de/umr/core/StandardGraphFactory.java
+++ src/de/umr/core/StandardGraphFactory.java
@@ -12,7 +12,7 @@ public class StandardGraphFactory {
      * @param size The number of vertices in the resulting graph.
      * @return A clique of requested size (a graph, in which any two vertices are connected by an edge).
      */
-    static MutableGraph<Integer> createClique(int size) {
+    public static MutableGraph<Integer> createClique(int size) {
         return GraphFileReader.graphByEdges(IntStream.range(0, size).boxed()     //[0, 1, ..., size-2, size-1]
                 .flatMap(i -> IntStream.range(0, i).boxed()
                         .map(j -> EndpointPair.ordered(i, j)))
@@ -23,7 +23,7 @@ public class StandardGraphFactory {
      * @param size The number of vertices in the resulting graph.
      * @return The graph consists exclusively of one circle (a path where the first and last vertex are equal).
      */
-    static MutableGraph<Integer> createCircle(int size) {
+    public static MutableGraph<Integer> createCircle(int size) {
         Preconditions.checkArgument(size > 1);
         return GraphFileReader.graphByEdges(IntStream.range(0, size).boxed()
                 .map(i -> EndpointPair.ordered(i, (i + 1) % size))
@@ -34,7 +34,7 @@ public class StandardGraphFactory {
      * @param size The number of vertices in the resulting graph.
      * @return The graph consists exclusively of one path. Thus it has exactly size-1 edges.
      */
-    static MutableGraph<Integer> createPath(int size) {
+    public static MutableGraph<Integer> createPath(int size) {
         return GraphFileReader.graphByEdges(IntStream.range(0, size - 1).boxed()
                 .map(i -> EndpointPair.ordered(i, i + 1))
                 .collect(Collectors.toList()));
@@ -45,7 +45,7 @@ public class StandardGraphFactory {
      * @return In the graph the vertex with ID 0 is connected to any other vertex, but no other edges exist.
      * The graph therefore forms a star-like figure with vertex 0 in the center.
      */
-    static MutableGraph<Integer> createStar(int size) {
+    public static MutableGraph<Integer> createStar(int size) {
         return GraphFileReader.graphByEdges(IntStream.range(1, size).boxed().map(i -> EndpointPair.ordered(0, i))
                 .collect(Collectors.toList()));
     }
