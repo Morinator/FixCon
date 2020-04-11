e2b1bb70511c67fcd61c440db24bf1e30abe4ce7
Morinator <mgross321@gmail.com>
2020-04-11 14:52:55 +0200

    renaming
    making fields final

--- src/de/umr/fixcon/wrappers/Solution.java
+++ src/de/umr/fixcon/wrappers/Solution.java
@@ -5,19 +5,26 @@ import com.google.common.graph.Graphs;
 
 import java.util.Set;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * This class is a wrapper for a solution. It contains:
+ * 1. The graph in which is searched for a subgraph that maximizes the target function
+ * 2. The subgraph, identified by its set of vertices, that is optimal.
+ * 3. The value of the target function for this specific subgraph.
+ */
 public class Solution {
 
-    private Graph<Integer> originalGraph;
-    private Set<Integer> subgraphSet = null;
+    private final Graph<Integer> graph;
+    private Set<Integer> subgraphSet;
     private double value = Integer.MIN_VALUE;
 
-    public Solution(Graph<Integer> originalGraph) {
-        this.originalGraph = originalGraph;
+    public Solution(final Graph<Integer> graph) {
+        this.graph = checkNotNull(graph);
     }
 
-
-    public void update(Set<Integer> subgraphSet, double value) {
-        this.subgraphSet = subgraphSet;
+    public void update(final Set<Integer> subgraphSet, final double value) {
+        this.subgraphSet = checkNotNull(subgraphSet);
         this.value = value;
     }
 
@@ -26,7 +33,7 @@ public class Solution {
     }
 
     public Graph<Integer> getSolution() {
-        return Graphs.inducedSubgraph(originalGraph, subgraphSet);
+        return Graphs.inducedSubgraph(graph, subgraphSet);
     }
 
 }
