0411a08f8ee3195c8e45330b364eaabae55245eb
Morinator <mgross321@gmail.com>
2020-04-20 03:04:10 +0200

    kotlified the SubIterator class

--- src/de/umr/fixcon/itarators/SubIterator.kt
+++ src/de/umr/fixcon/itarators/SubIterator.kt
@@ -1,47 +1,29 @@
-package de.umr.fixcon.itarators;
+package de.umr.fixcon.itarators
 
-import com.google.common.graph.Graph;
-import com.google.common.graph.Graphs;
-import com.google.common.graph.MutableGraph;
+import com.google.common.base.Preconditions
+import com.google.common.graph.Graph
+import com.google.common.graph.Graphs
+import com.google.common.graph.MutableGraph
 
-import static com.google.common.base.Preconditions.checkNotNull;
+class SubIterator(originalGraph: MutableGraph<Int>, private val targetSize: Int) : GraphIterator<Graph<Int>> {
+    private val originalGraph: MutableGraph<Int> = Preconditions.checkNotNull(originalGraph)
+    private var subgraphMutator = SubIterator_FromStart(originalGraph, anyVertex(), targetSize)
+    var searchTreeCounter = 0
+    var sizeKSubgraphCount = 0
 
-public final class SubIterator implements GraphIterator<Graph<Integer>> {
+    override fun isValid(): Boolean = subgraphMutator.isValid()
 
-    private final int targetSize;
-    private final MutableGraph<Integer> originalGraph;
-    public int searchTreeCounter = 0;
-    public int sizeKSubgraphCount = 0;
-    private SubIterator_FromStart subgraphMutator;
+    override fun current(): Graph<Int> = Graphs.copyOf(subgraphMutator.current())
 
-    public SubIterator(MutableGraph<Integer> originalGraph, int targetSize) {
-        this.originalGraph = checkNotNull(originalGraph);
-        this.targetSize = targetSize;
-        subgraphMutator = new SubIterator_FromStart(originalGraph, anyVertex(), targetSize);
-    }
-
-    @Override
-    public boolean isValid() {
-        return subgraphMutator.isValid();
-    }
-
-    @Override
-    public Graph<Integer> current() {
-        return Graphs.copyOf(subgraphMutator.current());
-    }
-
-    @Override
-    public void mutate() {    //fixed_subgraphIterator throws exception if it doesn't have next element
-        subgraphMutator.mutate();
-        sizeKSubgraphCount++;
-        while (!subgraphMutator.isValid() && originalGraph.nodes().size() > targetSize) {
-            searchTreeCounter += subgraphMutator.searchTreeCounter;
-            originalGraph.removeNode(subgraphMutator.startVertex());
-            subgraphMutator = new SubIterator_FromStart(originalGraph, anyVertex(), targetSize);
+    override fun mutate() { //fixed_subgraphIterator throws exception if it doesn't have next element
+        subgraphMutator.mutate()
+        sizeKSubgraphCount++
+        while (!subgraphMutator.isValid() && originalGraph.nodes().size > targetSize) {
+            searchTreeCounter += subgraphMutator.searchTreeCounter
+            originalGraph.removeNode(subgraphMutator.startVertex())
+            subgraphMutator = SubIterator_FromStart(originalGraph, anyVertex(), targetSize)
         }
     }
 
-    private int anyVertex() {
-        return originalGraph.nodes().iterator().next();
-    }
+    private fun anyVertex(): Int = originalGraph.nodes().first()
 }
