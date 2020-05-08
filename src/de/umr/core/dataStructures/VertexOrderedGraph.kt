d7b6e236be0a855fee0e81fe7deffc642831688f
Morinator <mgross321@gmail.com>
2020-05-08 16:57:32 +0200

    introduced size field for easier use

--- src/de/umr/core/dataStructures/VertexOrderedGraph.kt
+++ src/de/umr/core/dataStructures/VertexOrderedGraph.kt
@@ -4,7 +4,7 @@ import org.jgrapht.graph.DefaultEdge
 import org.jgrapht.graph.SimpleGraph
 import java.util.*
 
-class VertexOrderedGraph<E>: SimpleGraph<E, DefaultEdge>(DefaultEdge::class.java) {
+class VertexOrderedGraph<E> : SimpleGraph<E, DefaultEdge>(DefaultEdge::class.java) {
     private val insertionStack = LinkedList<E>()
 
     override fun addVertex(elem: E): Boolean {
@@ -23,4 +23,8 @@ class VertexOrderedGraph<E>: SimpleGraph<E, DefaultEdge>(DefaultEdge::class.java
         } else
             false
     }
+
+    val size: Int
+        get() = vertexSet().size
+
 }
