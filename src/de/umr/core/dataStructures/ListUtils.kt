4761e8201faf35f665c14483f8cfaed6ea807d1b
Morinator <mgross321@gmail.com>
2020-05-08 16:58:03 +0200

    methods are now extensions

--- src/de/umr/core/dataStructures/ListUtils.kt
+++ src/de/umr/core/dataStructures/ListUtils.kt
@@ -4,11 +4,9 @@ import java.util.*
 
 object ListUtils {
 
-    fun  incrementHead(list: MutableList<Int>) {
-        list[0] = list[0] + 1
+    fun MutableList<Int>.incrementHead() {
+        this[0] = this[0] + 1
     }
 
-    fun duplicateHead(list: Deque<Int>) {
-        list.push(list.peek())
-    }
+    fun <E> Deque<E>.duplicateHead() = this.push(this.peek())
 }
