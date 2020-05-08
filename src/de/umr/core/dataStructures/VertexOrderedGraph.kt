415bd04b1b7f3910b0afa80b4f3c2c92b16be591
Morinator <mgross321@gmail.com>
2020-05-08 03:04:43 +0200

    this data-structure keeps track of the insertion-order of the vertices in this graph

---
+++ src/de/umr/core/dataStructures/VertexOrderedGraph.kt
@@ -0,0 +1,26 @@
+package de.umr.core.dataStructures
+
+import org.jgrapht.graph.DefaultEdge
+import org.jgrapht.graph.SimpleGraph
+import java.util.*
+
+class VertexOrderedGraph<E>: SimpleGraph<E, DefaultEdge>(DefaultEdge::class.java) {
+    private val insertionStack = LinkedList<E>()
+
+    override fun addVertex(elem: E): Boolean {
+        return if (!vertexSet().contains(elem)) {
+            super.addVertex(elem)
+            insertionStack.push(elem)
+            true
+        } else
+            false
+    }
+
+    fun removeLastVertex(): Boolean {
+        return if (vertexSet().isNotEmpty()) {
+            super.removeVertex(insertionStack.pop())
+            true
+        } else
+            false
+    }
+}
