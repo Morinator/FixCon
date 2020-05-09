0a27e2102a8b79694540174f9fca800c6d01ba20
Morinator <mgross321@gmail.com>
2020-05-10 01:47:24 +0200

    documentation

--- src/de/umr/core/dataStructures/VertexOrderedGraph.kt
+++ src/de/umr/core/dataStructures/VertexOrderedGraph.kt
@@ -4,27 +4,37 @@ import org.jgrapht.graph.DefaultEdge
 import org.jgrapht.graph.SimpleGraph
 import java.util.*
 
-class VertexOrderedGraph<E> : SimpleGraph<E, DefaultEdge>(DefaultEdge::class.java) {
+/**
+ * A modified [SimpleGraph] that stores the inserted vertices in insertion-order.
+ * Therefore, [removeLastVertex] can be called.
+ */
+class VertexOrderedGraph<E>() : SimpleGraph<E, DefaultEdge>(DefaultEdge::class.java) {
+
     private val insertionStack = LinkedList<E>()
+
+    constructor(vararg elem: E) : this() {
+        elem.forEach { addVertex(it) }
+    }
 
     override fun addVertex(elem: E): Boolean {
         return if (!vertexSet().contains(elem)) {
             super.addVertex(elem)
             insertionStack.push(elem)
             true
-        } else
-            false
+        } else false
     }
 
+    /**
+     * Removes the last added vertex, if there are any vertices.
+     * @return **True** if the graph has changed as a result of the call, so if any vertices were present.
+     */
     fun removeLastVertex(): Boolean {
         return if (vertexSet().isNotEmpty()) {
             super.removeVertex(insertionStack.pop())
-            true
-        } else
-            false
+        } else false
     }
 
+    /**[size] is the number of vertices in the graph. Therefore it can't be negative.*/
     val size: Int
         get() = vertexSet().size
-
 }
