5b5045a444d070dd464305cc3946e64518ca7d58
Morinator <mgross321@gmail.com>
2020-05-10 01:49:00 +0200

    refactoring and renaming

--- src/de/umr/fixcon/itarators/SubIteratorFromStart.kt
+++ src/de/umr/fixcon/itarators/SubIteratorFromStart.kt
@@ -10,25 +10,24 @@ import org.jgrapht.Graphs.neighborSetOf
 import org.jgrapht.graph.DefaultEdge
 import java.util.*
 
-class SubIteratorFromStart(private val originalGraph: Graph<Int, DefaultEdge>, val startVertex: Int, private val targetSize: Int) : GraphIterator<Graph<Int, DefaultEdge>> {
-    private val subgraph = VertexOrderedGraph<Int>()
-    private val pointerStack: LinkedList<Int> = LinkedList(listOf(0))
 
+class SubIteratorFromStart(private val originalGraph: Graph<Int, DefaultEdge>, val startVertex: Int, private val targetSize: Int) : GraphIterator<Graph<Int, DefaultEdge>> {
+    private val subgraph = VertexOrderedGraph(startVertex)
+    private val pointerStack: LinkedList<Int> = LinkedList(listOf(0))
     private var extension: MultiStack<Int> = MultiStack(neighborSetOf(originalGraph, startVertex))
 
     init {
         require(targetSize > 1)
-        subgraph.addVertex(startVertex)
         mutate()
     }
 
-    override fun isValid(): Boolean = subgraph.size == targetSize
+    override fun isValid() = subgraph.size == targetSize
 
-    override fun current(): Graph<Int, DefaultEdge> = subgraph
+    override fun current() = subgraph
 
     override fun mutate() {
         do {
-            if (subgraph.size == targetSize) {
+            if (isValid()) {
                 deleteSubsetHead()
             } else {
                 if (pointerHeadIsOutOfRange()) { //size of subset is < k for following code:
@@ -46,34 +45,35 @@ class SubIteratorFromStart(private val originalGraph: Graph<Int, DefaultEdge>, v
     /*for the last element in the subset it is not necessary to adjust the extension-list, because the subset
     wont be extended further. Therefore in this case the adjustment of the extension-list is omitted.*/
     private fun addPivot() {
-        if (!subgraphOneTooSmall())
-            extension.addAll(getNewExtension(pivotVertex()))
-        addVertexWithEdges(pivotVertex())
+        expandExtension()
+        addVertexWithEdges(extension[pointerStack.first])
         pointerStack.incrementHead()
     }
 
     private fun addVertexWithEdges(vertex: Int) =
             neighborSetOf(originalGraph, vertex).filter { subgraph.containsVertex(it) }
-                    .forEach { addEdgeWithVertices(subgraph, pivotVertex(), it) }
-
-    private fun subgraphOneTooSmall() = subgraph.size == targetSize - 1
+                    .forEach { addEdgeWithVertices(subgraph, extension[pointerStack.first], it) }
 
     /*Because for the last element in the subset the extension-list was not adjusted, it also doesn't need
     to be delete here in this case. This is the case if the size of the subset is targetSize*/
     private fun deleteSubsetHead() {
-        if (!subgraphIsFull())
-            extension.removeTailSegment()
+        shrinkExtension()
         subgraph.removeLastVertex()
     }
 
-    private fun subgraphIsFull() = subgraph.size == targetSize
+    private fun expandExtension() {
+        if (subgraph.size != targetSize - 1)
+            extension.addAll(getNewExtension(extension[pointerStack.first]))
+    }
+
+    private fun shrinkExtension() {
+        if (!isValid()) extension.removeLastSegment()
+    }
 
     private fun getNewExtension(pivot_vertex: Int): List<Int> =
-            neighborSetOf(originalGraph, pivot_vertex).filter { !extension.contains(it) && it != startVertex }
+            neighborSetOf(originalGraph, pivot_vertex).filter { !extension.contains(it) && startVertex != it }
 
-    private fun pointerHeadIsOutOfRange(): Boolean = pointerStack.first == extension.size
+    private fun pointerHeadIsOutOfRange() = pointerStack.first == extension.size
 
-    private fun pointerHeadIsPending(): Boolean = subgraph.size == pointerStack.size
-
-    private fun pivotVertex(): Int = extension[pointerStack.first]
+    private fun pointerHeadIsPending() = subgraph.size == pointerStack.size
 }
