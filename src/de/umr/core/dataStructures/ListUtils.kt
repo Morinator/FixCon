dbe3ed5f116ac3e8315c2c81a5c94fc71749210f
Morinator <mgross321@gmail.com>
2020-05-10 01:42:21 +0200

    documentation

--- src/de/umr/core/dataStructures/ListUtils.kt
+++ src/de/umr/core/dataStructures/ListUtils.kt
@@ -2,11 +2,20 @@ package de.umr.core.dataStructures
 
 import java.util.*
 
+/**
+ * Provides extensions to the inbuilt types [MutableList] and [Deque].
+ */
 object ListUtils {
 
+    /**Increments the first entry (the one at index 0) in the given [MutableList].
+     * Example: [0, 2, 3].incrementHead() becomes [1, 2, 3]
+     */
     fun MutableList<Int>.incrementHead() {
         this[0] = this[0] + 1
     }
 
+    /**Duplicates the first element in the [Deque].
+     * Example: [1, 2, 5].duplicateHead() become [1, 1, 2, 5]
+     */
     fun <E> Deque<E>.duplicateHead() = this.push(this.peek())
 }
