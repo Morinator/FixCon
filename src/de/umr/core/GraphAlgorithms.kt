5fbda352d48228180b7fe4df024a380d2928a90f
Morinator <mgross321@gmail.com>
2020-05-10 01:47:34 +0200

    documentation

--- src/de/umr/core/GraphAlgorithms.kt
+++ src/de/umr/core/GraphAlgorithms.kt
@@ -6,11 +6,19 @@ import org.jgrapht.Graphs.neighborSetOf
 import org.jgrapht.graph.DefaultEdge
 import org.jgrapht.graph.SimpleGraph
 
+/**
+ * This class is stateless and only provides algorithms for
+ */
 object GraphAlgorithms {
-    fun hasTriangle(g: Graph<Int, DefaultEdge>) = g.vertexSet().any { x ->
+    /**
+     * @return **True** iff the graph contains at least 1 triangle
+     */
+    fun<E> hasTriangle(g: Graph<E, DefaultEdge>) = g.vertexSet().any { x ->
         neighborSetOf(g, x).any { y -> neighborSetOf(g, x).any { z -> g.containsEdge(y, z) } }
     }
 
+    /**
+     * @return A new Integer-valued graph, which is a copy of [g]*/
     fun copyIntGraph(g: Graph<Int, DefaultEdge>): Graph<Int, DefaultEdge> {
         val resultGraph = SimpleGraph<Int, DefaultEdge>(DefaultEdge::class.java)
         g.vertexSet().forEach {neighborSetOf(g, it).forEach { y -> addEdgeWithVertices(resultGraph, it, y) } }
