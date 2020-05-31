8101e3831b5887368a47a6cbff590932e544fc59
Morinator <mgross321@gmail.com>
2020-05-31 20:56:34 +0200

    now returns Solution instead of Int-Value

--- src/de/umr/fixcon/heuristics/LowerBoundGenerator.kt
+++ src/de/umr/fixcon/heuristics/LowerBoundGenerator.kt
@@ -2,38 +2,40 @@ package de.umr.fixcon.heuristics
 
 import de.umr.core.GraphAlgorithms.inducedSubgraph
 import de.umr.fixcon.wrappers.CFCO_Problem
+import de.umr.fixcon.wrappers.Solution
 import org.jgrapht.Graphs.neighborSetOf
+import org.jgrapht.graph.DefaultEdge
+import org.jgrapht.graph.SimpleGraph
 import kotlin.math.log2
-import kotlin.math.max
 import kotlin.math.roundToInt
 
 object LowerBoundGenerator {
 
     private const val runCountMultiplicand = 100
 
-    fun getBound(problem: CFCO_Problem, runs: Int = runCountMultiplicand * log2(problem.originalGraph.size.toDouble()).roundToInt()): Int {
+    fun getBound(problem: CFCO_Problem, runs: Int = runCountMultiplicand * log2(problem.originalGraph.size.toDouble()).roundToInt()): Solution {
         require(problem.originalGraph.size >= problem.targetSize) { "Target-size may not be smaller than graph" }
-        var bestLowerBound = Int.MIN_VALUE
-        repeat(runs) { bestLowerBound = max(bestLowerBound, singleRun(problem)) }
-        return bestLowerBound.also { println("generated the lower bound is $bestLowerBound in $runs runs") }
+        val currentBestSolution = Solution(SimpleGraph(DefaultEdge::class.java), Int.MIN_VALUE)
+        repeat(runs) {
+            currentBestSolution.updateIfBetter(singleRun(problem))
+        }
+        return currentBestSolution.also { println("generated the lower bound is $currentBestSolution in $runs runs") }
     }
 
-    private fun singleRun(problem: CFCO_Problem): Int {
+    private fun singleRun(problem: CFCO_Problem): Solution {
         val startVertex = problem.originalGraph.vertexSet().random()
         val subgraphSet = HashSet<Int>(setOf(startVertex))
         val extensionSet = HashSet<Int>(neighborSetOf(problem.originalGraph, startVertex))
 
         repeat(problem.targetSize - 1) {
-            if (extensionSet.isEmpty()) return Int.MIN_VALUE
+            if (extensionSet.isEmpty()) return Solution(SimpleGraph(DefaultEdge::class.java), Int.MIN_VALUE)
             val nextVertex = extensionSet.random()
             subgraphSet += nextVertex
             extensionSet -= nextVertex
-            neighborSetOf(problem.originalGraph, startVertex)
-                    .filter { !subgraphSet.contains(it) }
-                    .forEach { extensionSet.add(it) }
-            //couldn't generate a subgraph of right size
+            extensionSet.addAll(neighborSetOf(problem.originalGraph, startVertex) - subgraphSet)
         }
 
-        return problem.function.eval(inducedSubgraph(problem.originalGraph, subgraphSet), problem.parameters)
+        val subGraph = inducedSubgraph(problem.originalGraph, subgraphSet)
+        return Solution(subGraph, problem.function.eval(subGraph, problem.parameters))
     }
 }
