d830974f4293a696a8866c094801a86089750f9e
Morinator <mgross321@gmail.com>
2020-06-02 13:39:28 +0200

    this class takes a map which contains probability-weights for its keys and then can return a random element based on these weights

---
+++ src/de/umr/fixcon/heuristics/WeightedRandomCollection.kt
@@ -0,0 +1,29 @@
+package de.umr.fixcon.heuristics
+
+import java.util.*
+
+/**
+ * @param weightMap Specifies for each *key* the weighted probability for picking this element. In other
+ * words: The probability of picking an element is proportional to the value saved in this map.
+ */
+class WeightedRandomCollection(weightMap: Map<Int, Double>) {
+    private val map: NavigableMap<Double, Int> = TreeMap()
+
+    private var totalWeight = 0.0
+
+    init {
+        weightMap.entries.forEach { entry -> add(entry.key, entry.value) }
+    }
+
+    private fun add(result: Int, weight: Double): WeightedRandomCollection {
+        require(weight >= 0) { "Element cannot have a negative weight" }
+        if (weight == 0.0) return this
+
+        totalWeight += weight
+        map[totalWeight] = result
+        return this
+    }
+
+    fun pickRandom(): Int = map.higherEntry(Random().nextDouble() * totalWeight).value
+
+}
