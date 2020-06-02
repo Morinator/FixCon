c226a475ef647baa2623d9fe9cc8e00e454176e1
Morinator <mgross321@gmail.com>
2020-06-02 13:39:52 +0200

    daskfdfr

--- src/de/umr/fixcon/heuristics/LowerBoundGenerator.kt
+++ src/de/umr/fixcon/heuristics/LowerBoundGenerator.kt
@@ -1,39 +1,54 @@
 package de.umr.fixcon.heuristics
 
 import de.umr.core.GraphAlgorithms.inducedSubgraph
+import de.umr.fixcon.heuristics.vertexPickers.DensePicker
+import de.umr.fixcon.heuristics.vertexPickers.LaplacePicker
+import de.umr.fixcon.heuristics.vertexPickers.SparsePicker
+import de.umr.fixcon.heuristics.vertexPickers.VertexPicker
 import de.umr.fixcon.wrappers.CFCO_Problem
 import de.umr.fixcon.wrappers.Solution
 import org.jgrapht.Graphs.neighborSetOf
 import kotlin.math.log2
 import kotlin.math.roundToInt
 
-object LowerBoundGenerator {
+class LowerBoundGenerator(var problem: CFCO_Problem) {
 
-    private const val runCountMultiplicand = 100
+    private val runCountMultiplicand = 5  //only based off experience
 
-    fun getBound(problem: CFCO_Problem, runs: Int = runCountMultiplicand * log2(problem.originalGraph.size.toDouble()).roundToInt()): Solution {
+    fun getBound(runs: Int = getRunCount()): Solution {
+
         require(problem.originalGraph.size >= problem.targetSize) { "Target-size may not be smaller than graph" }
-        val currentBestSolution = Solution()
-        repeat(runs) {
-            currentBestSolution.updateIfBetter(singleRun(problem))
-        }
-        return currentBestSolution.also { println("generated the lower bound is $currentBestSolution in $runs runs") }
+
+        val bestLaplaceSolution = takeBestSolution(LaplacePicker)
+        val bestDenseSolution = takeBestSolution(DensePicker)
+        val bestSparseSolution = takeBestSolution(SparsePicker)
+
+        println("heuristic runs: $runs")
+        println("laplace " + bestLaplaceSolution.value)
+        println("dense " + bestDenseSolution.value)
+        println("sparse " + bestSparseSolution.value)
+
+        return listOf(bestLaplaceSolution, bestDenseSolution, bestSparseSolution).maxBy { it.value }!!
     }
 
-    private fun singleRun(problem: CFCO_Problem): Solution {
-        val startVertex = problem.originalGraph.vertexSet().random()
+    private fun generateConnectedSubgraph(picker: VertexPicker): Solution {
+        val startVertex = picker.startVertex(problem.originalGraph)
         val subgraphSet = HashSet<Int>(setOf(startVertex))
         val extensionSet = HashSet<Int>(neighborSetOf(problem.originalGraph, startVertex))
 
-        repeat(problem.targetSize - 1) {
+        while (subgraphSet.size != problem.targetSize) {
             if (extensionSet.isEmpty()) return Solution()
-            val nextVertex = extensionSet.random()
+            val nextVertex = picker.extensionVertex(problem.originalGraph, subgraphSet, extensionSet)
             subgraphSet += nextVertex
             extensionSet -= nextVertex
-            extensionSet.addAll(neighborSetOf(problem.originalGraph, startVertex) - subgraphSet)
+            extensionSet.addAll(neighborSetOf(problem.originalGraph, nextVertex) - subgraphSet)
         }
 
         val subGraph = inducedSubgraph(problem.originalGraph, subgraphSet)
         return Solution(subGraph, problem.function.eval(subGraph, problem.parameters))
     }
+
+    private fun getRunCount() = log2(problem.originalGraph.size.toDouble()).roundToInt() * problem.targetSize * runCountMultiplicand
+
+    private fun takeBestSolution(picker: VertexPicker) = (1..getRunCount()).map { generateConnectedSubgraph(picker) }.maxBy { it.value }!!
 }
