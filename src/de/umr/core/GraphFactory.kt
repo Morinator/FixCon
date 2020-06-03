bddad126f83341cf0a14ae25a7641fcccc0aabc0
Morinator <mgross321@gmail.com>
2020-06-03 15:28:31 +0200

    now uses ranges instead of IntStream

--- src/de/umr/core/GraphFactory.kt
+++ src/de/umr/core/GraphFactory.kt
@@ -4,9 +4,6 @@ package de.umr.core
 import de.umr.core.GraphFileReader.defaultWeightedGraphByEdges
 import de.umr.core.dataStructures.VertexOrderedGraph
 import org.jgrapht.Graph
-import java.util.stream.Collectors.toList
-import java.util.stream.IntStream
-import kotlin.streams.toList
 
 /**
  * This object class provides factory methods for 4 common types of graphs: {clique, circle, path, star}.
@@ -19,9 +16,7 @@ object GraphFactory {
      * @return A clique of requested size (a graph, in which any two vertices are connected by an edge).
      */
     fun createClique(size: Int): VertexOrderedGraph<Int> =
-            defaultWeightedGraphByEdges(IntStream.range(0, size).boxed() //[0, 1, ..., size-2, size-1]
-                    .flatMap { IntStream.range(0, it).boxed().map { x -> it to x } }
-                    .collect(toList()))
+            defaultWeightedGraphByEdges((0 until size).flatMap { (0 until it).map { x -> Pair(it, x) } })
 
     /**
      * @param size The number of vertices in the resulting [Graph].
@@ -29,24 +24,19 @@ object GraphFactory {
      */
     fun createCircle(size: Int): VertexOrderedGraph<Int> {
         require(size > 1)
-        return defaultWeightedGraphByEdges(IntStream.range(0, size).toList()
-                .map { it to (it + 1) % size })
+        return defaultWeightedGraphByEdges((0 until size).map { Pair(it, (it + 1) % size) })
     }
 
     /**
      * @param size The number of vertices in the resulting [Graph].
      * @return The graph consists exclusively of one path. Thus it has exactly size-1 edges.
      */
-    fun createPath(size: Int): VertexOrderedGraph<Int> =
-            defaultWeightedGraphByEdges(IntStream.range(0, size - 1).toList()
-                    .map { it to it + 1 })
+    fun createPath(size: Int): VertexOrderedGraph<Int> = defaultWeightedGraphByEdges((0 until size - 1).map { it to it + 1 })
 
     /**
      * @param size The number of vertices in the resulting [Graph].
      * @return In the graph the vertex with ID 0 is connected to any other vertex, but no other edges exist.
      * The graph therefore forms a star-like figure with vertex 0 in the center.
      */
-    fun createStar(size: Int): VertexOrderedGraph<Int> =
-            defaultWeightedGraphByEdges(IntStream.range(1, size).toList()
-                    .map { 0 to it })
+    fun createStar(size: Int): VertexOrderedGraph<Int> = defaultWeightedGraphByEdges((1 until size).map { Pair(0, it) })
 }
