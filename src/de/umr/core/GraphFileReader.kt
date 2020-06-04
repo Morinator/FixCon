6e6839f7c4128a4768be63372bc85d3f871a59d8
Morinator <mgross321@gmail.com>
2020-06-04 20:26:07 +0200

    refactoring

--- src/de/umr/core/GraphFileReader.kt
+++ src/de/umr/core/GraphFileReader.kt
@@ -1,7 +1,6 @@
 package de.umr.core
 
 import de.umr.core.dataStructures.VertexOrderedGraph
-import org.jgrapht.Graphs.addEdgeWithVertices
 import java.lang.Integer.parseInt
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -9,10 +8,14 @@ import kotlin.streams.toList
 
 object GraphFileReader {
 
+    private val weightedEdgePattern = Regex("""\d+\s+\d+\s+\d+""")
+    private val unweightedEdgePattern = Regex("""\d+\s+\d+""")
+    private val separator = Regex("""\s+""")
+
     private fun edgesFromNetworkRepo(filePath: String, weighted: Boolean = false): List<Triple<Int, Int, Double>> {
         return Files.lines(Paths.get(filePath)).toList()
-                .filter { it.matches(if (weighted) Regex("""\d+\s+\d+\s+\d+""") else Regex("""\d+\s+\d+""")) }
-                .map { it.split(Regex("""\s+""")) }
+                .filter { it.matches(if (weighted) weightedEdgePattern else unweightedEdgePattern) }
+                .map { it.split(separator) }
                 .map { Triple(parseInt(it[0]), parseInt(it[1]), if (weighted) it[2].toDouble() else 1.0) }
     }
 
