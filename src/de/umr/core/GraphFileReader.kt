a19bee44aa8c55cd53cde2a13a2b5ead45d5e164
Morinator <mgross321@gmail.com>
2020-06-05 01:45:49 +0200

    uses expression body

--- src/de/umr/core/GraphFileReader.kt
+++ src/de/umr/core/GraphFileReader.kt
@@ -12,12 +12,11 @@ object GraphFileReader {
     private val unweightedEdgePattern = Regex("""\d+\s+\d+""")
     private val separator = Regex("""\s+""")
 
-    private fun edgesFromFile(filePath: String, weighted: Boolean = false): List<Triple<Int, Int, Double>> {
-        return Files.lines(Paths.get(filePath)).toList()
-                .filter { it.matches(if (weighted) weightedEdgePattern else unweightedEdgePattern) }
-                .map { it.split(separator) }
-                .map { Triple(parseInt(it[0]), parseInt(it[1]), if (weighted) it[2].toDouble() else 1.0) }
-    }
+    private fun edgesFromFile(filePath: String, weighted: Boolean = false): List<Triple<Int, Int, Double>> =
+            Files.lines(Paths.get(filePath)).toList()
+                    .filter { it.matches(if (weighted) weightedEdgePattern else unweightedEdgePattern) }
+                    .map { it.split(separator) }
+                    .map { Triple(parseInt(it[0]), parseInt(it[1]), if (weighted) it[2].toDouble() else 1.0) }
 
     fun graphFromFile(filePath: String, weighted: Boolean = false): VertexOrderedGraph<Int> =
             VertexOrderedGraph(edgesFromFile(filePath, weighted))
