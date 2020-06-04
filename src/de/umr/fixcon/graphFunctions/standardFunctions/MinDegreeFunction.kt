5b9bf5d8697aef5e986bab5d63059dbf2ef7ef45
Morinator <mgross321@gmail.com>
2020-06-04 20:59:55 +0200

    refactoring möglich da if in kotlin ein ausdruck ist

--- src/de/umr/fixcon/graphFunctions/standardFunctions/MinDegreeFunction.kt
+++ src/de/umr/fixcon/graphFunctions/standardFunctions/MinDegreeFunction.kt
@@ -4,6 +4,7 @@ import de.umr.core.dataStructures.VertexOrderedGraph
 import de.umr.fixcon.graphFunctions.GraphFunction
 import org.jgrapht.Graph
 import org.jgrapht.graph.DefaultEdge
+import java.lang.IllegalArgumentException
 
 /**
  * Returns the minimum degree of all vertices in this graph.
@@ -13,11 +14,8 @@ object MinDegreeFunction : GraphFunction {
 
     override fun completeAdditionBound(subgraph: VertexOrderedGraph<Int>, targetSize: Int, args: List<Int>) = targetSize - subgraph.vertexCount
 
-    override fun eval(g: Graph<Int, DefaultEdge>, args: List<Int>): Int {
-        if (g.vertexSet().isEmpty())
-            throw Exception("graph ist leer")
-        return g.vertexSet().map { g.degreeOf(it) }.min()!!
-    }
+    override fun eval(g: Graph<Int, DefaultEdge>, args: List<Int>): Int =
+             if (g.vertexSet().isEmpty()) throw IllegalArgumentException("Graph is empty") else g.vertexSet().map { g.degreeOf(it) }.min()!!
 
     override fun globalUpperBound(vararg size: Int): Int = (size.first() - 1)
 }
