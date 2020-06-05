3607fe1dd4097e59e4d0e40c630d27cddaedd626
Morinator <mgross321@gmail.com>
2020-06-05 17:46:37 +0200

    uses Kotlins readLines

--- src/de/umr/core/GraphFileReader.kt
+++ src/de/umr/core/GraphFileReader.kt
@@ -1,20 +1,17 @@
 package de.umr.core
 
 import de.umr.core.dataStructures.VertexOrderedGraph
+import java.io.File
 import java.lang.Integer.parseInt
-import java.nio.file.Files
-import java.nio.file.Paths
-import kotlin.streams.toList
 
 private val weightedEdgePattern = Regex("""\d+\s+\d+\s+\d+""")      //3 separated Ints
 private val unweightedEdgePattern = Regex("""\d+\s+\d+""")          //2 separated Ints
 private val separator = Regex("""\s+""")                            //some whitespace
 
-private fun edgesFromFile(filePath: String, weighted: Boolean = false): List<Triple<Int, Int, Double>> =
-        Files.lines(Paths.get(filePath)).toList()
-                .filter { it.matches(if (weighted) weightedEdgePattern else unweightedEdgePattern) }
-                .map { it.split(separator) }
-                .map { Triple(parseInt(it[0]), parseInt(it[1]), if (weighted) it[2].toDouble() else 1.0) }
+private fun edgesFromFile(filePath: String, weighted: Boolean = false) = File(filePath).readLines()
+        .filter { it.matches(if (weighted) weightedEdgePattern else unweightedEdgePattern) }
+        .map { it.split(separator) }
+        .map { Triple(parseInt(it[0]), parseInt(it[1]), if (weighted) it[2].toDouble() else 1.0) }
 
 /**Returns a [VertexOrderedGraph] based off the text-file that's present at [filePath]. It uses the adjacency-list
  * format from NetworkRepository. If [weighted] is *false*, all edges have a default weight of 1.0*/
