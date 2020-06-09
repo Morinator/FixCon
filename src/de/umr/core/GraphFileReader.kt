4459d396c42881435beaa25688369f95976c9af0
Morinator <mgross321@gmail.com>
2020-06-09 18:34:52 +0200

    added kdoc now takes argument "allowLoops"

--- src/de/umr/core/GraphFileReader.kt
+++ src/de/umr/core/GraphFileReader.kt
@@ -8,11 +8,23 @@ private val unweightedEdgePattern = Regex("""\d+\s+\d+""")          //2 separate
 private val weightedEdgePattern = Regex("""\d+\s+\d+\s+\d+""")      //3 separated Ints
 private val separator = Regex("""\s+""")                            //some whitespace
 
-private fun edgesFromFile(filePath: String, weighted: Boolean = false) = File(filePath).readLines()
+/**
+ * @param filePath The path of the graph file that is read in.
+ * @param weighted True iff it should read in the third number in each line as the weight of the edge.
+ * @param allowLoops If true: Allows lines in which the first and second number is the same,
+ * as this would encode a self-loop in the graph.
+ *
+ * @return A [List] of [Triple]. Each triple corresponds to one edge. The first and second entry in the triple is
+ * the IDs of the vertices the edge connects. The
+ */
+fun edgesFromFile(filePath: String, weighted: Boolean = false, allowLoops: Boolean = false)
+        = File(filePath).readLines()
         .filter { it.matches(if (weighted) weightedEdgePattern else unweightedEdgePattern) }
         .map { it.split(separator) }
+        .filter { if (allowLoops) true else it[0] != it[1] }
         .map { Triple(parseInt(it[0]), parseInt(it[1]), if (weighted) it[2].toDouble() else 1.0) }
 
 /**Returns a [VertexOrderedGraph] based off the text-file that's present at [filePath]. It uses the adjacency-list
  * format from NetworkRepository. If [weighted] is *false*, all edges have a default weight of 1.0*/
-fun graphFromFile(filePath: String, weighted: Boolean = false) = VertexOrderedGraph(edgesFromFile(filePath, weighted))
+fun graphFromFile(filePath: String, weighted: Boolean = false) =
+        VertexOrderedGraph(edgesFromFile(filePath, weighted))
