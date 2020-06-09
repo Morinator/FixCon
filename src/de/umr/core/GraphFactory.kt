51bcb4d65177c3b263d794336f0bed79b6e9b4ae
Morinator <mgross321@gmail.com>
2020-06-09 19:47:05 +0200

    renaming

--- src/de/umr/core/GraphFactory.kt
+++ src/de/umr/core/GraphFactory.kt
@@ -1,6 +1,6 @@
 package de.umr.core
 
-import de.umr.core.dataStructures.VertexOrderedGraph as graph
+import de.umr.core.dataStructures.VertexOrderedGraph as vog
 
 /**The vertices of the returned graphs are Int-IDs. Generic vertices would need further info for constructors.
  * Each method has a Int-parameter which is the number of vertices in the returned graph.*/
@@ -8,15 +8,18 @@ import de.umr.core.dataStructures.VertexOrderedGraph as graph
 /**All edges in the returning graphs have the default weight 1.0*/
 private const val defaultWeight: Double = 1.0
 
+/**Turns the pair of vertices into a weighted edge with [defaultWeight]*/
+private fun withWeight(x: List<Pair<Int, Int>>) = x.map { Triple(it.first, it.second, defaultWeight) }
+
 /**@return A clique of requested size (a graph, in which any two vertices are connected by an edge).*/
-fun createClique(numVertices: Int) = graph((0 until numVertices).flatMap { x -> (0 until x).map { y -> Triple(x, y, defaultWeight) } })
+fun createClique(numVertices: Int) = vog(withWeight((0 until numVertices).flatMap { x -> (0 until x).map { y -> Pair(x, y) } }))
 
 /**@return The graph consists exclusively of one circle (a path where the first and last vertex are equal).*/
-fun createCircle(numVertices: Int) = graph((0 until numVertices).map { Triple(it, (it + 1) % numVertices, defaultWeight) })
+fun createCircle(numVertices: Int) = vog(withWeight((0 until numVertices).map { Pair(it, (it + 1) % numVertices) }))
 
 /**@return The graph consists exclusively of one path. Thus it has exactly size-1 edges.*/
-fun createPath(numVertices: Int) = graph((0 until numVertices - 1).map { Triple(it, it + 1, defaultWeight) })
+fun createPath(numVertices: Int) = vog(withWeight((0 until numVertices - 1).map { Pair(it, it + 1) }))
 
 /**@return In the graph the vertex with ID 0 is connected to any other vertex, but no other edges exist.
  * The graph therefore forms a star-like figure with vertex 0 in its center.*/
-fun createStar(numVertices: Int) = graph((1 until numVertices).map { Triple(0, it, defaultWeight) })
+fun createStar(numVertices: Int) = vog(withWeight((1 until numVertices).map { Pair(0, it) }))
