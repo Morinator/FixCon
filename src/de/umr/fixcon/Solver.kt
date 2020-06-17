4dd2e9ef8349e012d5dfd4afe36a806faaa2a133
Morinator <mgross321@gmail.com>
2020-06-18 01:29:53 +0200

    debugging prints

--- src/de/umr/fixcon/Solver.kt
+++ src/de/umr/fixcon/Solver.kt
@@ -1,7 +1,7 @@
 package de.umr.fixcon
 
+import de.umr.core.printFullAnalysis
 import de.umr.core.vertexCount
-import de.umr.fixcon.heuristics.someSolution
 import de.umr.fixcon.itarators.SubIterator
 import de.umr.fixcon.wrappers.Problem
 import de.umr.fixcon.wrappers.Solution
@@ -9,28 +9,36 @@ import de.umr.removeSmallComponents
 
 class Solver<V>(private val problem: Problem<V>) {
 
-    fun solve(): Solution<V> {
-
+    init {
+        val oldVertexCount = problem.g.vertexCount
         removeSmallComponents(problem.g, problem.k)
+        println("Clearing components:".padEnd(25) + "${oldVertexCount - problem.g.vertexCount} vertices \n")
+        printFullAnalysis(problem.g)
+    }
 
-        val s = Solution<V>()
+    var verticesDeleted = 0
+    private val s = Solution<V>()
+    var iter = subIterAtAnyVertex()
 
-        fun subIterAtAnyVertex() = SubIterator(problem, problem.g.vertexSet().random(), s)
-
-        var iter = subIterAtAnyVertex()
-
-        fun updateStartVertexIfNeeded() {
-
-            while (!iter.isValid && problem.g.vertexCount > problem.k) {
-                problem.g.removeVertex(iter.startVertex)
-                iter = subIterAtAnyVertex()
-            }
-        }
+    fun solve(): Solution<V> {
 
         while (s.value < problem.globalOptimum && iter.isValid) {
             iter.mutate()
             updateStartVertexIfNeeded()
         }
+
+        println("Failed SubIterators:".padEnd(25) + verticesDeleted)
+
         return s
+    }
+
+    private fun subIterAtAnyVertex() = SubIterator(problem, problem.g.vertexSet().first(), s)
+
+    private fun updateStartVertexIfNeeded() {
+        while (!iter.isValid && problem.g.vertexCount > problem.k) {
+            verticesDeleted++
+            problem.g.removeVertex(iter.startVertex)
+            iter = subIterAtAnyVertex()
+        }
     }
 }
