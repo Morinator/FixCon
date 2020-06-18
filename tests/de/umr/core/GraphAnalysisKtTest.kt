db841cfa319e2cc568ec3044d08cc04cfc42afe3
Morinator <mgross321@gmail.com>
2020-06-18 02:24:23 +0200

    more tests

---
+++ tests/de/umr/core/GraphAnalysisKtTest.kt
@@ -0,0 +1,25 @@
+package de.umr.core
+
+import de.umr.GraphFile
+import de.umr.GraphFile.BioYeast
+import de.umr.core.io.graphFromFile
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class GraphAnalysisKtTest {
+
+    private val delta = 0.0001
+
+    @Test
+    fun degreeShareMap_test() {
+        val m = degreeShareMap(createStar(4))
+        assertEquals(3.0 / 4, m[1]!!, delta)
+        assertEquals(1.0 / 4, m[3]!!, delta)
+    }
+
+    @Test   //checks if no error is thrown on execution
+    fun testPrintFullAnalysis_test() {
+        printFullAnalysis(BioYeast)
+        printFullAnalysis(graphFromFile(BioYeast))
+    }
+}
