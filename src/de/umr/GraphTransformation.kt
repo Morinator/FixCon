370611aefe8c6abc7b08f630b07473adfbe4bdb7
Morinator <mgross321@gmail.com>
2020-06-19 01:45:11 +0200

    added function "removeVerticesByPredicate" with one test so far

--- src/de/umr/GraphTransformation.kt
+++ src/de/umr/GraphTransformation.kt
@@ -1,5 +1,6 @@
 package de.umr
 
+import de.umr.core.openNB
 import org.jgrapht.Graph
 import org.jgrapht.alg.connectivity.ConnectivityInspector
 import org.jgrapht.graph.DefaultEdge
@@ -8,5 +9,19 @@ fun <V> removeSmallComponents(g: Graph<V, DefaultEdge>, threshold: Int) {
     ConnectivityInspector(g).connectedSets().forEach {
         if (it.size < threshold)
             g.removeAllVertices(it)
+    }
+}
+
+fun <V> removeVerticesByPredicate(g: Graph<V, DefaultEdge>, predicate: (V) -> Boolean) {
+    val badVertices: MutableSet<V> = g.vertexSet().filter { predicate(it) }.toMutableSet()
+    while (badVertices.isNotEmpty()) {
+        val next = badVertices.random()
+        val neighbours = g.openNB(next)
+
+        g.removeVertex(next)
+        badVertices.remove(next)
+        
+        badVertices.addAll(neighbours.filter { predicate(it) })
+        badVertices.removeAll(neighbours.filter { !predicate(it) })
     }
 }
---
+++ tests/de/umr/GraphTransformationKtTest.kt
@@ -0,0 +1,58 @@
+package de.umr
+
+import de.umr.core.addEdgeWithVertices
+import de.umr.core.dataStructures.VertexOrderedGraph
+import de.umr.core.io.graphFromFile
+import de.umr.core.vertexCount
+import org.jgrapht.alg.connectivity.ConnectivityInspector
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+internal class GraphTransformationKtTest {
+
+    @Nested
+    internal inner class removeSmallComponents_test {
+        @Test
+        fun tree_test() {
+            val g = graphFromFile(GraphFile.CustomTree)
+            val size = g.vertexCount
+            removeSmallComponents(g, 3)
+            assertEquals(size, g.vertexCount)
+        }
+
+        @Test
+        fun graph_3_times_5_test() {
+            val g = VertexOrderedGraph<Int>()
+            for (i in 0..20 step 10)
+                (i + 1 until i + 5).forEach { g.addEdgeWithVertices(i, it) }
+
+            assertEquals(15, g.vertexCount)
+            removeSmallComponents(g, 5)
+            assertEquals(15, g.vertexCount)
+            removeSmallComponents(g, 6)
+            assertEquals(0, g.vertexCount)
+        }
+
+        @Test
+        fun oneSmallComponent() {
+            val g = graphFromFile(GraphFile.CustomTree)
+            (101..105).forEach { g.addEdgeWithVertices(100, it) }
+            assertEquals(2, ConnectivityInspector(g).connectedSets().size)
+            removeSmallComponents(g, 10)
+            assertTrue(ConnectivityInspector(g).isConnected)
+        }
+    }
+
+    @Nested
+    internal inner class removeVerticesByPredicate_test {
+
+        @Test
+        fun tree_test() {
+            val g = graphFromFile(GraphFile.CustomTree)
+            removeVerticesByPredicate(g) { g.degreeOf(it) == 1 }
+            assertEquals(1, g.vertexCount)
+        }
+    }
+}
