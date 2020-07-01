235a887bdc6b882321f65d50173909be09c2fca7
Morinator <mgross321@gmail.com>
2020-07-01 10:36:02 +0200

    better use of Kotlin API -> shortened

--- src/de/umr/core/GraphAnalysis.kt
+++ src/de/umr/core/GraphAnalysis.kt
@@ -8,18 +8,16 @@ import org.jgrapht.graph.DefaultEdge
 
 private const val defaultTableSize = 10
 
-fun <V> degreeShareMap(g: Graph<V, DefaultEdge>) = HashMap<Int, Double>().apply {
-    g.vertexSet().forEach { put(g.degreeOf(it), getOrDefault(g.degreeOf(it), 0.0) + 1.0) }
-    entries.forEach { put(it.key, it.value / g.vertexCount) }
-}
+fun <V> degreeShareMap(g: Graph<V, DefaultEdge>) = g.vertexSet()
+        .groupingBy { g.degreeOf(it) }.eachCount()
+        .mapValues { 1.0 * it.value / g.vertexCount }
 
-fun<V> printFullAnalysis(g: Graph<V, DefaultEdge>, tableSize: Int = defaultTableSize) {
+fun <V> printFullAnalysis(g: Graph<V, DefaultEdge>, tableSize: Int = defaultTableSize) {
     println("Vertices:".padEnd(pad) + g.vertexCount)
     println("Edges:".padEnd(pad) + g.edgeCount)
     println("Max-Degree:".padEnd(pad) + g.degreeSequence.max()!!)
     println("\nDegree-Freq:")
-    val m = degreeShareMap(g)
-    m.entries.sortedBy { it.value }.reversed().take(tableSize).forEach {
+    degreeShareMap(g).entries.sortedByDescending { it.value }.take(tableSize).forEach {
         println(it.key.toString().padEnd(pad) + "%.3f".format(it.value))
     }
     println()
