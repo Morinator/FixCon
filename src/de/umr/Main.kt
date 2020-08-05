8cd81a9cbfb9e6800c8c937f24a3988e69e31fd0
Morinator <mgross321@gmail.com>
2020-08-05 23:41:55 +0200

    better fileReader more stuff in main

--- src/de/umr/Main.kt
+++ src/de/umr/Main.kt
@@ -1,10 +1,11 @@
 package de.umr
 
+import de.umr.core.io.graphFromFile
 import de.umr.fixcon.Problem
 import de.umr.fixcon.graphFunctions.graphFunctionByID
+import de.umr.fixcon.solve
 
 fun main(args: Array<String>) {
-
     val fu = graphFunctionByID(args[1].toInt(), args[2].split(",").map { it.toInt() }.toList())
-
+    println(solve(Problem(graphFromFile(args[3]), fu)))
 }
--- src/de/umr/core/io/GraphFileReader.kt
+++ src/de/umr/core/io/GraphFileReader.kt
@@ -6,16 +6,23 @@ import de.umr.core.fromWeightedEdges
 import java.io.File
 import java.lang.Integer.parseInt
 
-private val separator = Regex("""\s+""")        //some whitespace
-private val vertex = Regex("""\w+""")           //at least one alphanumeric character
+private val separator = Regex("""\s+""")
+private val vertex = Regex("""\w+""")
 
 private val unweightedEdge = Regex("""$vertex$separator$vertex""")
 private val weightedEdge = Regex("""$unweightedEdge$separator$vertex""")
 
-fun edgesFromFile(file: GraphFile, allowLoops: Boolean = false) = File(file.path).readLines().asSequence().drop(file.skipLines)
-        .filter { it.matches(if (file.weighted) weightedEdge else unweightedEdge) }
+fun edgesFromFile(file: String, allowLoops: Boolean = false, skipLines: Int = 0, weighted: Boolean = false) = File(file).readLines().asSequence().drop(skipLines)
+        .filter { it.matches(if (weighted) weightedEdge else unweightedEdge) }
         .map { it.split(separator) }
         .filter { it[0] != it[1] || allowLoops }
-        .mapTo(ArrayList(), { Triple(parseInt(it[0]), parseInt(it[1]), if (file.weighted) it[2].toDouble() else defaultEdgeWeight) })
+        .mapTo(ArrayList(), { Triple(parseInt(it[0]), parseInt(it[1]), if (weighted) it[2].toDouble() else defaultEdgeWeight) })
+
+fun graphFromFile(file: String, allowLoops: Boolean = false, skipLines: Int = 0, weighted: Boolean = false) =
+        fromWeightedEdges(edgesFromFile(file, allowLoops, skipLines, weighted))
+
+
+
+fun edgesFromFile(file: GraphFile, allowLoops: Boolean = false) = edgesFromFile(file.path, allowLoops, file.skipLines, file.weighted)
 
 fun graphFromFile(f: GraphFile) = fromWeightedEdges(edgesFromFile(f))
