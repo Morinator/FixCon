e24396ef72917b1fe2994574753eb9dcd20c542f
Moritz <mgross321@gmail.com>
2020-08-06 12:00:15 +0200

    adapted main method

--- src/de/umr/Main.kt
+++ src/de/umr/Main.kt
@@ -5,14 +5,15 @@ import de.umr.fixcon.Problem
 import de.umr.fixcon.graphFunctions.graphFunctionByID
 import de.umr.fixcon.solve
 
-/**args[0] == k
- * args[1] == function ID
- * args[2] == parameters for function, separated by ",". The parameters 3 and 5 for are "3,5" for example.
- * args[3] == File-Path for the graph.
- * args[4] == lines to skip in the graph-file (for example due to comments in the file).
+/**args[0] == File-Path for the graph.
+ * args[1] == k
+ * args[2] == functionID, param1, param2, ..., paramN-1, paramN
  */
 fun main(args: Array<String>) {
-    val fu = graphFunctionByID(args[1].toInt(), args[0].toInt(), args[2].split(",").map { it.toInt() }.toList())
-    val g = graphFromFile(args[3], skipLines = args[4].toInt())
-    println(solve(Problem(g, fu)))
+    val k = args[1].toInt()
+    val funcID = args[2].split(",").first().toInt()
+    val funcParams = args[2].split(",").drop(1).map { it.toInt() }
+
+    val fu = graphFunctionByID(funcID, k, funcParams)
+    println(solve(Problem(graphFromFile(args[0]), fu)))
 }
