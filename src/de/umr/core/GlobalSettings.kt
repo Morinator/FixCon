a0ad25a5c75fdfdf966449761a1f3482c779488f
Moritz <mgross321@gmail.com>
2020-08-06 13:23:53 +0200

    deleted everything

--- src/de/umr/core/GlobalSettings.kt
+++
@@ -1,8 +0,0 @@
-package de.umr.core
-
-const val defaultEdgeWeight = 1.0
-const val pad = 30                      //padding to the right that is used for print-debugging
-
-const val useHeuristic = false
-const val useCliqueJoinRule = false
-const val useAdditionBound = true
